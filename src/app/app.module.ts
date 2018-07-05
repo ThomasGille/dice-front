9d97dc83f9ea30f267a29fd3b369f6f60bf84684
ThomasGille <totogille@gmail.com>
2018-07-05 18:08:13 +0200

    before major dice refacto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule} from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule, MatInputModule } from '@angular/material';
+import { DiceComponent } from './dice/dice.component';
+import { DiceManagerComponent } from './dice-manager/dice-manager.component';
+
+import { MonsterService } from './service/monsters.service';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DiceComponent,
+    DiceManagerComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatCardModule,
+    MatInputModule,
   ],
-  providers: [],
+  exports: [
+    MatCardModule,
+    MatInputModule,
+  ],
+  providers: [
+    MonsterService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
