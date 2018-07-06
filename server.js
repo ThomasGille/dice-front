26a616ae1daac3720ba799269cd8b7aac121be41
ThomasGille <totogille@gmail.com>
2018-07-06 15:00:49 +0200

    fith try to prod

--- server.js
+++ server.js
@@ -24,15 +24,8 @@ const forceSSL = function() {
   
 app.use(forceSSL());
 
-// For all GET requests, send back index.html
-// so that PathLocationStrategy can be used
-
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
 // Run the app by serving the static files
 // in the dist directory
-
 app.use(express.static(__dirname + '/dist'));
 
 // Start the app by listening on the default
